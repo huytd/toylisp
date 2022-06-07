102ecca11cf133e40720995821612b20aed96341
Huy Tran <kingbazoka@gmail.com>
2022-06-06 23:16:31 -0700

    WIP

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import { parse } from './parser';
 import fs from 'fs';
 import { IntegerNode, SeqNode, StringNode, SyntaxNode } from './parser/types';
 
+
 const ARITHMETIC = {
     '+': 'ADD',
     '-': 'SUB',
@@ -50,14 +51,58 @@ export class LookupTable {
     }
 }
 
-export class Compiler {
-    constants = new LookupTable();
+export enum OpCode {
+    NOP = 0,
+    LOAD_CONST = 1,
+    LOAD_NAME,
+    STORE_NAME,
+    PRINT,
+    CMP,
+    JMP,
+    JMP_IF_FALSE
+}
+
+/*
+Program is an object that hold the compiled program
+It has a list of instructions. Each instruction is a pack of
+three numbers: [label, opcode, operand]
+
+If not specified, the default label is 0.
+
+The Program will have the push and read, read_by_label methods. The read
+method return the instruction at the index i, and the read_by_label return
+the instruction with a matching label. (to use in JMP, for example)
+
+Also, we need to modify the current program to output a Program object
+instead of an array of annotated code. In the unit tests, we can werite
+a disassembler to convert the code from Program into a readable array.
+*/
+
+export type Instruction = [number, number, number];
+
+export class Program {
+    instructions: Instruction[];
+    labels = new LookupTable();
     symbols = new LookupTable();
-    program: SeqNode[];
+    constants = new LookupTable();
+
+    constructor() {
+        this.instructions = [];
+        this.labels.write("");
+    }
+
+    push(opcode: OpCode, operand: number, label?: string) {
+        const labelId = label ? this.labels.read(label) : 0;
+        this.instructions.push([labelId, opcode, operand]);
+    }
+}
+
+export class Compiler {
+    source: SeqNode[];
     labelCount = 0;
 
-    constructor(program: SeqNode[]) {
-        this.program = program;
+    constructor(source: SeqNode[]) {
+        this.source = source;
     }
 
     compileNode(node: SyntaxNode, storing?: boolean) {
@@ -67,11 +112,11 @@ export class Compiler {
                 case 'let': {
                     const store_name = this.compileNode(node.value[1], true);
                     const load_const = this.compileNode(node.value[2]);
-                    return [ load_const, store_name];
+                    return [load_const, store_name];
                 }
                 case 'print': {
                     const load_const = this.compileNode(node.value[1]);
-                    return [ load_const, "PRINT" ];
+                    return [load_const, "PRINT"];
                 }
                 case '+': case '-': case '*': case '/': case '%': {
                     const load_const_a = this.compileNode(node.value[1]);
@@ -80,7 +125,7 @@ export class Compiler {
                     return [load_const_a, load_const_b, operator];
                 }
                 case '=': case '>': case '<': case '<=':
-                    case '>=': case '!=': case 'eq': case 'neq': {
+                case '>=': case '!=': case 'eq': case 'neq': {
                     const load_const_a = this.compileNode(node.value[1]);
                     const load_const_b = this.compileNode(node.value[2]);
                     return [load_const_a, load_const_b, "CMP"];
@@ -113,19 +158,11 @@ export class Compiler {
             const id = this.constants.read((node as StringNode).value);
             return "LOAD_CONST " + id;
         }
-        return [ "unknown" ];
+        return ["unknown"];
     };
 
     compile() {
-        return this.program.map(seq => this.compileNode(seq));
-    }
-
-    getConstants() {
-        return this.constants;
-    }
-
-    getSymbols() {
-        return this.symbols;
+        return this.source.map(seq => this.compileNode(seq));
     }
 }
 
@@ -136,8 +173,8 @@ const debugRun = () => {
     console.table(program.map(line => SourcePrinter(line)));
     const result = compiler.compile().map(line => line.flat().join(","));
     console.table(result);
-    compiler.getConstants().debug();
-    compiler.getSymbols().debug();
+    // compiler.getConstants().debug();
+    // compiler.getSymbols().debug();
 };
 
 if (process.env.NODE_ENV !== "test") {
